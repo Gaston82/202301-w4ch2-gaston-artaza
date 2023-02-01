b6eeabed0167957e59136cddf337d201bfbc58ed
Gaston82 <cabezaartaza@hotmail.com>
2023-02-01 02:50:15 +0100

    change bootstrap import from app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "./components/Form/Form";
 import { getRandomGifs } from "./services";
 
