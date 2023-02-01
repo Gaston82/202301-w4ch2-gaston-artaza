7fb8f659ea3313bd3d082de85be764f9a3982c52
Gaston82 <cabezaartaza@hotmail.com>
2023-02-01 02:01:36 +0100

    add handle category

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "./components/Form/Form";
 import { getRandomGifs } from "./services";
 
 const App = () => {
   const [category, setCategory] = useState("");
-  const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState("");
 
   useEffect(() => {
     const fetchGifs = async () => {
-      const result = await getRandomGifs();
+      const result = await getRandomGifs(category);
       setGifs(result);
     };
     fetchGifs();
-  }, []);
+  }, [category]);
 
   const handleCategory = (query: string) => {
     setCategory(query);
@@ -23,11 +24,7 @@ const App = () => {
       <Form onCategory={handleCategory} />
       <div className="row">
         <div className="container-gif offset-1 col-6">
-          <img
-            src="https://tse3.mm.bing.net/th?id=OIP.Mt1FIGttFtgk2JdDD-2xQAHaEK&pid=Api&P=0"
-            className="gif"
-            alt="el pity"
-          />
+          <img src={gifs} className="gif" alt="el pity" />
         </div>
       </div>
       <div className="row">
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,11 @@
 const API_KEY = "8vG6z2FesxcljPGMEGNRT0hP79exFH21";
 
-export const getRandomGifs = async () => {
+export const getRandomGifs = async (category: string) => {
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&q="gatos"&tag=&rating=g`
+    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&q="${category}"&tag=&rating=g`
   );
 
   const gifs = await response.json();
 
-  return gifs;
+  return gifs.data.images.fixed_height.url;
 };
